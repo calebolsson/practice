e22151936365d8a0a1c3c68702e3698f965f622e
FWitBees <131329541+FWitBees@users.noreply.github.com>
2023-09-23 11:02:54 -0500

    Add files via upload

---
+++ dicegame.c
@@ -0,0 +1,24 @@
+#include "dicegame.h"
+
+int getRandomNumber(int min, int max) {
+    //your code goes here
+    return 0; //erase this
+}
+
+ROUNDTYPE getRoundType() {
+    //your code goes here
+    return REGULAR; //erase this
+}
+
+int getRoundPoints(ROUNDTYPE roundType) {
+    //your code goes here
+    return 0; //erase this
+}
+
+void printPlayerPoints(int p1, int p2) {
+    //your code goes here
+}
+
+void printRoundInfo(ROUNDTYPE t, int dice, int points) {
+    //your code goes here
+}
